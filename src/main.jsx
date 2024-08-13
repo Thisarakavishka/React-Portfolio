74e7a34c0e077c0a2102908a22ac3a09ae2e1b5e
thisarakavishka <thisarakavishka0@gmail.com>
2024-08-13 21:42:58 +0530

    add navigation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import "@fontsource/roboto"
+import "@fontsource/outfit"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
