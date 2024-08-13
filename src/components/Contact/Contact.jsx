1c97775fb4bf896c11518e3d3fb8f0090d562417
thisarakavishka <thisarakavishka0@gmail.com>
2024-08-13 21:47:34 +0530

    add footer of portfolio

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css"
+
+function Contact() {
+    return (
+        <footer id='contact' className={styles.container}>
+            <div className={styles.text}>
+                <h2>Contact</h2>
+                <p>Feel free to hire</p>
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/emailIcon.png")} alt='email Icon' />
+                    <a href='mailto:myemail@email.com'>thisarakavishka0@gmail.com</a>
+                </li>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/linkedinIcon.png")} alt='linkedin Icon' />
+                    <a href='https://www.linkedin.com/in/thisara-kavishka/'>linkedin.com/in/thisara-kavishka</a>
+                </li>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/githubIcon.png")} alt='github Icon' />
+                    <a href='https://github.com/Thisarakavishka'>github.com/Thisarakavishka</a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Contact
